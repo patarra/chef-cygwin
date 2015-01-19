4f48a1671cc27e22b0087e4c6905a4c089712e1d
José Manuel Felguera Rodríguez <jmfelguera@MacBook-Air.local>
2015-01-19 10:32:58 +0100

    http_proxy shouldn't use the scheme following the cygwin documentation However, the http_proxy should contain scheme http when it is used in knife bootstrap

--- libraries/cygwin_helpers.rb
+++ libraries/cygwin_helpers.rb
@@ -1,7 +1,7 @@
 module Cygwin
 	module Helpers
         def proxy_command
-            Chef::Config['http_proxy'].nil? ? "" : "--proxy #{Chef::Config['http_proxy']}"
+          Chef::Config['http_proxy'].nil? ? "" : "--proxy #{Chef::Config['http_proxy'].gsub(/http(s)?:\/\// ,'')}"
         end
     end
 end
